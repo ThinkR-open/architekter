d516cc224822d7b8068f5e5851f8537ab2647d1f
margot <margot@thinkr.fr>
2022-04-01 20:09:15 +0000

    Fix CI
    Why:
    
    *
    
    This change addresses the need by:
    
    *
    
    Issues
    Issue #22

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(architekter)
-library(showtext)
-
-font_add(family = "Roboto", system.file("fonts", "Roboto.ttf", package = "architekter"))
-font_add(family = "Roboto Light", system.file("fonts", "RobotoLight.ttf", package = "architekter"))
 
 test_check("architekter")
